e7ad6ed9c249069339869f43881cee87c6a61f91
root <root@7085bb8e535f>
2023-03-12 19:47:39 +0000

    lotter

---
+++ src/_01_random/_6_lottery_numbers/Lotterynumbers.java
@@ -0,0 +1,32 @@
+package _01_random._6_lottery_numbers;
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class Lotterynumbers {
+	public static void main(String[] args) {
+		
+	
+Random lottery = new Random();
+int lotter1 = lottery.nextInt(100); {
+	JOptionPane.showMessageDialog(null, "Hello");
+}
+int lotter2 = lottery.nextInt(100); {
+	JOptionPane.showMessageDialog(null, "Ta da");
+}
+int lotter3 = lottery.nextInt(100); {
+	JOptionPane.showMessageDialog(null, "Woo hoo");
+}
+int lotter4 = lottery.nextInt(100); {
+	JOptionPane.showMessageDialog(null, "Good job");
+}
+int lotter5 = lottery.nextInt(100); {
+	JOptionPane.showMessageDialog(null, "Nice");
+}
+int lotter6 = lottery.nextInt(100); {
+	JOptionPane.showMessageDialog(null, "Were set");
+}
+
+}
+}
